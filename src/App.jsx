7dda4fee0908b339f04e8b7706e73d953f33e562
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2025-03-22 14:01:05 +0200

    bento grid applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import "./App.css";
+import { Bento } from "../src/layouts/Bento";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [verse, setVerse] = useState("");
+  const retrieveTheRandomBibleVerse = () => {
+    axios
+      .get("https://bible-api.com/data/web/random")
+      .then((response) => {
+        console.log(
+          response.data.random_verse.book +
+            " " +
+            response.data.random_verse.chapter +
+            ":" +
+            response.data.random_verse.verse
+        );
+        setVerse(
+          response.data.random_verse.book +
+            " " +
+            response.data.random_verse.chapter +
+            ":" +
+            response.data.random_verse.verse
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
-      </div>
+      <button onClick={retrieveTheRandomBibleVerse}>
+        Get Random Bible Verse
+      </button>
+      {verse && <h1>{verse}</h1>}
+      <Bento></Bento>
     </>
-  ) 
+  );
 }
 
 export default App;
