47d6fd0408fc869d9d63bb31602d52350265bbf0
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2025-03-23 05:23:05 +0200

    404 - NotFoundPage

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { Bento } from "./layouts/Bento.jsx";
+import { NotFoundPage } from "./components/NotFoundPage.jsx";
 
 const router = createBrowserRouter([
-  {path: "/", element: <App />},
-  {path: "/home", element: <Bento />},
-  {path: "*", element: <NotFoundPage />}
-])
+  { path: "/", element: <App /> },
+  { path: "/home", element: <Bento /> },
+  { path: "*", element: <NotFoundPage /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
